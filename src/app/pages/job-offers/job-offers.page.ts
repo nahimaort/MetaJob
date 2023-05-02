37f543125738f89c29f73082fc63585dea110394
Nahima Ortega <nahimaortega@gmail.com>
2023-05-02 20:39:32 +0100

    Filters included

--- src/app/pages/job-offers/job-offers.page.ts
+++ src/app/pages/job-offers/job-offers.page.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./job-offers.page.scss'],
 })
 export class JobOffersPage implements OnInit {
+  profileImg: string = '../assets/images/profile-pic.jpeg';
+  filters: string[] = ['All', 'Remote', 'Applied', 'Freshers', 'Fulltime', 'Partime'];
+  selectedFilter: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.selectedFilter = this.filters[0];
   }
 
 }
