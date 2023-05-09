2bb0a058727d20c194c2b86eb8a862d1eb6f8485
Simon Rusu <simonrust97@gmail.com>
2023-05-09 21:07:04 +0100

    fix: :bug: fixed some visual bugs for android emulation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCa9sqfPCh7dll61bHHMhawG4JF2viptU0",
+    authDomain: "metajob-3e0dc.firebaseapp.com",
+    databaseURL: "https://metajob-3e0dc-default-rtdb.firebaseio.com",
+    projectId: "metajob-3e0dc",
+    storageBucket: "metajob-3e0dc.appspot.com",
+    messagingSenderId: "1042486827527",
+    appId: "1:1042486827527:web:05b4dea03fd152912c28a2",
+    measurementId: "G-SCB2KJXY6F"
+  }
 };
